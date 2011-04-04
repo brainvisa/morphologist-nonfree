edd2d4c7437e29c8afe4ca2d01ab888f0d3273ea
clarafischer <clarafischer@users.noreply.github.com>
2011-04-04 14:53:23 +0000

    New function for the topological classification.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/t1mri/t1mri-private/trunk@41916 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- vip/src/libvip/topology/topology_clas.c
+++ vip/src/libvip/topology/topology_clas.c
@@ -202,3 +202,63 @@ int outside)
  VipFreeVolume(temp);
  return(OK);
 }
+
+/*----------------------------------------------------------------------------*/
+int VipTopologicalClassificationForTwoLabel(
+Volume *vol,
+int inside,
+int outside)
+/*----------------------------------------------------------------------------*/
+
+{
+  VipOffsetStruct *vos;
+  Vip_S16BIT *imptr, *tempptr;
+  int i, j, k;
+  Topology26Neighborhood *topo26;
+  Volume *temp;
+
+  if (VipVerifyAll(vol)==PB)
+    {
+      VipPrintfExit("(topology)VipTopologicalClassificationForTwoLabel");
+      return(PB);
+    }
+  if (VipTestType(vol,S16BIT)!=OK)
+    {
+      VipPrintfError("Sorry,  VipTopologicalClassificationForTwoLabel is only implemented for S16BIT volume");
+      VipPrintfExit("(topology)VipTopologicalClassificationForTwoLabel");
+      return(PB);
+    }
+ if (mVipVolBorderWidth(vol) < 1)
+    {
+      VipPrintfError("Sorry, VipTopologicalClassificationForTwoLabel is only implemented with border");
+      VipPrintfExit("(topology)VipTopologicalClassificationForTwoLabel");
+      return(PB);
+    }
+
+ temp = VipCopyVolume(vol,"classif");
+ if(temp==PB) return(PB);
+
+ vos = VipGetOffsetStructure(vol);
+ if(vos==PB) return(PB);
+
+ imptr = VipGetDataPtr_S16BIT(vol)+vos->oFirstPoint;
+ tempptr = VipGetDataPtr_S16BIT(temp)+vos->oFirstPoint;
+
+ VipSetBorderLevel( vol, outside ); /*place object in non label area*/
+
+ topo26 = VipCreateTopology26Neighborhood( vol);
+  if(topo26==PB) return(PB);
+
+ for (k=mVipVolSizeZ(vol); k--; imptr+=vos->oLineBetweenSlice, tempptr +=vos->oLineBetweenSlice)
+   for (j=mVipVolSizeY(vol); j--; imptr+=vos->oPointBetweenLine,tempptr+=vos->oPointBetweenLine)
+     for (i=mVipVolSizeX(vol); i--; imptr++, tempptr++)
+       { 
+	 if(*imptr!=inside && *imptr!=outside)
+	   *tempptr = VipComputeTopologicalClassificationForTwoLabel_S16BIT(topo26,imptr,inside,outside);
+       } 
+
+ if(VipTransferVolumeData(temp,vol)==PB) return(PB);
+
+ VipFreeVolume(temp);
+ return(OK);
+}
